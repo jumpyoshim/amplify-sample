b84cac4dbdd82d4fdcb5f03b510eb957140783ab
jumpyoshim <jumpyoshim@gmail.com>
2019-12-30 16:06:45 +0900

    feat: add analytics

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-on:click="sendEvent">Send event</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Analytics } from "aws-amplify";
+window.LOG_LEVEL = "VERBOSE";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "analytics",
+  data() {
+    return {};
+  },
+  methos: {
+    sendEvent() {
+      Analytics.record({
+        name: "Button Clicked",
+        attributes: {
+          AttrName1: "AttrValue1",
+          AttrName2: "AttrValue2"
+        }
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import awsconfig from "./aws-exports";
+import Amplify, * as AmplifyModules from "aws-amplify";
+import { AmplifyPlugin } from "aws-amplify-vue";
+
+Amplify.configure(awsconfig);
+Vue.use(AmplifyPlugin, AmplifyModules);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
